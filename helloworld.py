1c92ea635c5e15caee8f75245a27442335bde4f7
annox <annox4uk@gmail.com>
2021-07-28 15:19:14 +0700

    Test autodeploy 1

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     if request.method == "GET":
-        return ('Hello world 1 and test UPDATE')
+        return ('Hello world 1')
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 8080)
