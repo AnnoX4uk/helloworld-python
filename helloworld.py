f78f7598dc0878eecafc59169b16082a68d300e5
annox <annox4uk@gmail.com>
2021-07-30 15:28:52 +0700

    add point

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from flask import Flask, request
+import sys
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     if request.method == "GET":
-        print ('hello! Im a point!')
+        sys.stdout('This is a point')
         return ('Hello world 1')
 
 if __name__ == "__main__":
